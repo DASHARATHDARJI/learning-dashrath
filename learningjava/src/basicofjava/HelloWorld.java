6a7b56952215707ac0c0f8a04d00a6016bf266da
dashrath <darjidashrath57@gmail.com>
2022-06-19 22:19:29 +0530

    Basic Java Program

---
+++ learningjava/src/basicofjava/HelloWorld.java
@@ -0,0 +1,12 @@
+package basicofjava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HelloWorld");
+		
+	}
+
+}
