94ec3e8eb21f697bb8ed9afe95edec3cca2bbb93
dashrath <darjidashrath57@gmail.com>
2022-06-22 22:43:01 +0530

    Impliment loops ifelse switch statments

---
+++ learningjava/src/basicofjava/IfEleseCondition.java
@@ -0,0 +1,20 @@
+package basicofjava;
+
+public class IfEleseCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 27;
+		
+		
+		if(age > 25) {
+			System.out.println("My Age Is Not Correct");
+		}
+		else {
+			System.out.println("My Age Is  Correct");
+		}
+		
+	}
+	
+}
