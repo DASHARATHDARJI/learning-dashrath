9de287a24b2abb1f3a34f84bb43d172c80d79df6
dashrath <darjidashrath57@gmail.com>
2022-06-26 14:06:53 +0530

    ContinueStatement

---
+++ learningjava/src/loops/ContinueStatement.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class ContinueStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=1;
+		
+		for (i=1; i<=10; i++) {
+		if (i==7){
+		continue;
+		}
+		System.out.print(i);
+		System.out.println();
+		}		
+	}
+}
+
