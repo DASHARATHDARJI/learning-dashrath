c7d20504ee06b544c8db021cc95fee83b24e3e7e
dashrath <darjidashrath57@gmail.com>
2022-06-26 13:54:48 +0530

    break statement

---
+++ learningjava/src/loops/Breakandcontiune.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class Breakandcontiune {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int i=1;
+		
+		for (i=1; i<=10; i++) {
+			
+			System.out.print(i);
+		if(i==7)
+			break;
+		System.out.println();
+		}
+		
+	}
+}
