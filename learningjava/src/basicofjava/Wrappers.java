d6a50364ac2c2ad58faa7ffa97c09c8ff8faad25
dashrath <darjidashrath57@gmail.com>
2022-07-19 13:13:05 +0530

    Wrapper class

---
+++ learningjava/src/basicofjava/Wrappers.java
@@ -0,0 +1,16 @@
+package basicofjava;
+
+public class Wrappers {
+
+	public static void main(String[] args) {
+	
+	int i = 100;	
+		
+	Integer j = new Integer( i ); 
+	
+	System.out.println(i == j);
+		
+
+	}
+
+}
